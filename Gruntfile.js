bb5162cdf22fa0124f5b3001c528c62145375bfe
Reinier Ladan <reinierl@gmail.com>
2014-02-22 15:53:10 +0100

    Added a grunt task to compile the sass to css and put it in the public dir of Laravel for testing.

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+		        files: {
+		            './public/style/thepanel.css': './public/style/thepanel.scss',
+		            '../../../public/packages/hidiyo/thepanel/style/thepanel.css': './public/style/thepanel.scss'
+		        }
+		    }
+	    },
+		watch: {
+			css: {
+				files: ['**/*.scss'],
+				tasks: ['sass']
+			},
+			reload: {
+				files: ['**/*.blade.php','**/*.scss'],
+				tasks: ['reload']
+			},
+			options: {
+		      forever: true,
+		    },
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	grunt.registerTask('watchreload', ['watch']);
+	grunt.registerTask('default',['watch:css']);
+
+	grunt.registerTask("reload", "reload Chrome on OS X",
+	function() {
+		require("child_process").exec("osascript " +
+			"-e 'tell application \"Google Chrome\" " +
+			"to tell the active tab of its first window' " +
+			"-e 'reload' " +
+			"-e 'end tell'");
+	});
+};
